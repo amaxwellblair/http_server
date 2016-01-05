2a058604d7f82ffdeff4817341bd937cf2737cd6
Allan <amaxwellblair@gmail.com>
2016-01-04 20:12:47 -0700

    iteration 2

--- lib/ruby_built_server.rb
+++ lib/ruby_built_server.rb
@@ -1,10 +1,13 @@
 require 'socket'
 require 'hurley'
+require 'time'
 # http://127.0.0.1:9292
 
 tcp_server = TCPServer.new(9292)
 # client = tcp_server.accept
 i = -1
+circuitbreaker = 0
+response_count = -1
 while true
   client = tcp_server.accept
   puts "Ready for a request"
@@ -17,16 +20,6 @@ while true
   puts request_lines.inspect
 
   puts "Sending response."
-
-
-  # response = "<pre>" + "Hello, World! (#{i += 1})" + "</pre>"
-
-  # GET / HTTP/1.1
-  # +User-Agent: Hurley v0.2
-  # +Accept-Encoding: gzip;q=1.0,deflate;q=0.6,identity;q=0.3
-  # +Accept: */*
-  # +Connection: close
-  # +Host: 127.0.0.1:9292
 
   first_line = request_lines[0].split(" ")
   verb = first_line[0]
@@ -41,17 +34,33 @@ while true
   origin = sixth_line[1]
   port = sixth_line[2]
 
-  response =        ("<pre>\n" +
-                     "Verb: #{verb}\n" +
+  request_parse =   ("Verb: #{verb}\n" +
                      "Path: #{path}\n" +
                      "Protocol: #{protocol}\n" +
                      "Host: #{host}\n" +
                      "Port: #{port}\n" +
                      "Origin: #{origin}\n" +
-                     "Accept: #{accept}\n" +
-                     "</pre>")
+                     "Accept: #{accept}\n")
+  response_count += 1
+  if path == "/"
+    response = request_parse
+  elsif path == "/hello"
+    response = "Hello, World! (#{i += 1})\n" + request_parse
+  elsif path == "/hello_restart"
+    i = -1
+    response = request_parse
+  elsif path == "/datetime"
+    response = "#{Time.new.strftime("%I:%M%p on %A, %B %d, %Y")}\n" + request_parse
+  elsif path == "/shutdown"
+    response = "Total Requests: #{response_count}\n" + request_parse
+    circuitbreaker = 1
+  elsif path == "/shutdown_restart"
+    response_count = -1
+  end
 
-  output = "<html><head></head><body>#{response}</body></html>"
+
+
+  output = "<html><head></head><body><pre>#{response}</pre></body></html>"
   headers = ["http/1.1 200 ok",
             "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
             "server: ruby",
@@ -63,9 +72,9 @@ while true
   puts ["Wrote this response:", headers, output].join("\n")
 
   client.close
-  puts "\nResponse complete, exiting."
+  puts "\nResponse complete"
 
-  if response == "shut down\n"
+  if circuitbreaker == 1
     break
   end
 end
--- test/http_server_test.rb
+++ test/http_server_test.rb
@@ -3,53 +3,111 @@ SimpleCov.start
 require 'hurley'
 require 'socket'
 require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
 require 'pry'
+require 'time'
 
 class HttpServerTest < Minitest::Test
-  attr_reader :client, :iterator
+  attr_reader :client, :debug_info, :html_begin, :html_end
+
+  def self.test_order
+    :alpha
+  end
 
   def setup
     @client = Hurley::Client.new "http://127.0.0.1:9292"
-    @iterator = 0
+    @debug_info = (   "Verb: GET\n" +
+                      "Path: /\n" +
+                      "Protocol: HTTP/1.1\n" +
+                      "Host:  127.0.0.1\n" +
+                      "Port: 9292\n" +
+                      "Origin:  127.0.0.1\n" +
+                      "Accept: */*\n" )
+    @html_begin = "<html><head></head><body><pre>"
+    @html_end = "</pre></body></html>"
   end
+
+  def parse_helper(path)
+    @debug_info = (    "Verb: GET\n" +
+                      "Path: #{path}\n" +
+                      "Protocol: HTTP/1.1\n" +
+                      "Host:  127.0.0.1\n" +
+                      "Port: 9292\n" +
+                      "Origin:  127.0.0.1\n" +
+                      "Accept: */*\n" )
+  end
+
+  def time_helper
+    time_is_now = Time.new.strftime("%I:%M%p on %A, %B %d, %Y")
+  end
+
+  def hello_helper
+    path = "/hello_restart"
+    response = client.get path do |req|
+      req.url
+    end
+  end
+
+  def response_helper
+    path = "/shutdown_restart"
+    response = client.get path do |req|
+      req.url
+    end
+  end
+
+  # def restart_helper
+  #   `ruby ./lib/ruby_built_server.rb`
+  # end
+
 
   def test_response_success
     response = Hurley.get("http://127.0.0.1:9292")
     assert response.success?
   end
 
-  def test_hello_world_counter
-    skip
+  def test_root_path
     response = client.get "" do |req|
       req.url
     end
-    assert_equal "<html><head></head><body><pre>Hello, World! (#{@iterator += 1})</pre></body></html>", response.body
+    assert_equal html_begin + debug_info + html_end, response.body
   end
 
-  def test_parse_request
-    response = client.get "" do |req|
+  def test_hello_path
+    hello_helper
+    path = "/hello"
+    3.times do |i|
+      response = client.get path do |req|
+        req.url
+      end
+      parse_helper(path)
+      assert_equal html_begin + "Hello, World! (#{i})\n#{debug_info}" + html_end, response.body
+    end
+  end
+
+  def test_date_time
+    path = "/datetime"
+    response = client.get path do |req|
       req.url
     end
-    parsed_request = ("<html><head></head><body><pre>\n" +
-                      "Verb: GET\n" +
-                      "Path: /\n" +
-                      "Protocol: HTTP/1.1\n" +
-                      "Host:  127.0.0.1\n" +
-                      "Port: 9292\n" +
-                      "Origin:  127.0.0.1\n" +
-                      "Accept: */*\n" +
-                      "</pre></body></html>")
-    assert_equal parsed_request, response.body
+    parse_helper(path)
+    assert_equal html_begin + "#{time_helper}\n#{debug_info}" + html_end, response.body
   end
 
-
-  # def test_custom_request
-  #   skip
-  #   k = client.get "" do |req|
-  #     # binding.pry
-  #     req.url
-  #   end
-  #   assert_equal "Hello", k.body.chomp
-  # end
+  def test_shut_down
+    response_helper
+    path = "/hello"
+    3.times do |i|
+      response = client.get path do |req|
+        req.url
+      end
+    end
+    path = "/shutdown"
+    response = client.get path do |req|
+      req.url
+    end
+    parse_helper(path)
+    assert_equal html_begin + "Total Requests: #{3}\n#{debug_info}" + html_end, response.body
+  end
 
 end
