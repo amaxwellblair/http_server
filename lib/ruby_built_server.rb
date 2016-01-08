6a0442ed2da1f9eaee6c5596fd53ab4c75d1fcc2
Allan <amaxwellblair@gmail.com>
2016-01-07 21:48:10 -0700

    small change to server spacing

--- lib/ruby_built_server.rb
+++ lib/ruby_built_server.rb
@@ -12,8 +12,6 @@ tcp_server = TCPServer.new(ARGV[0].to_i)
 
 response = Response.new
 
-#Request (Take Input) -> Do Something -> Response (Send Output)
-
 begin
   while true
     server = Server.new(tcp_server)
@@ -23,14 +21,12 @@ begin
     response.request = request
     output = response.body
     headers = Header.new(request, response, URL)
-    # path = PathHandler.new(request)
-    # server.send(path.response)
     server.send_response(output, headers)
     break if server.shut_down?(output)
   end
 
 rescue => error
-  raw_request[0] = raw_request[0].sub("/force_error", "/error_back_trace")
+  raw_request[0] = raw_request[0].gsub(/\/\S+/, "/error_back_trace")
   request = Request.new(raw_request)
   response.request = request
   response.error_back_trace(error.backtrace.join("\n"))
