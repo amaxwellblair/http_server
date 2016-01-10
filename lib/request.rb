fe6cdb7ec5a8adac056e04d6e72a8f1aad7eaac5
Allan <amaxwellblair@gmail.com>
2016-01-10 14:20:03 -0700

    nothing

--- lib/request.rb
+++ lib/request.rb
@@ -1,8 +1,8 @@
 class Request
-  attr_accessor :request
+  attr_accessor :raw_header
 
-  def initialize(request)
-    @request = request
+  def initialize(raw_header)
+    @raw_header = raw_header
     @path = 0
   end
 
@@ -26,16 +26,18 @@ class Request
     "Accept: #{accept}\n"
   end
 
+  # ["GET / HTTP/1.1", "Host: 127.0.0.1:9292", "Accept-Encoding: gzip, deflate", "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9", "Accept-Language: en-us", "Cache-Control: max-age=0", "Connection: keep-alive"]
+
   def first_line
-    request[0].split(" ")
+    raw_header[0].split(" ")
   end
 
   def fourth_line
-    request[3].split(": ")
+    raw_header[3].split(": ")
   end
 
   def sixth_line
-    request[5].split(":")
+    raw_header[5].split(":")
   end
 
   def verb
@@ -52,8 +54,8 @@ class Request
   end
 
   def body
-    return nil if request.last.split("?")[1].nil?
-    param_find_all(request.last.split("?")[1])
+    return nil if raw_header.last.split("?")[1].nil?
+    param_find_all(raw_header.last.split("?")[1])
   end
 
   def protocol
